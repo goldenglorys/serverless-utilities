0cc8872c43a5cc7391ed295000f2b5f46bc584de
goldenglorys <olusolagloryolamide@gmail.com>
2021-12-29 22:48:47 +0100

    Add post req

--- api/index.py
+++ api/index.py
@@ -1,15 +1,30 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import json
+
+
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    # def do_GET(self):
+    #     s = self.path
+    #     dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    #     self.send_response(200)
+    #     self.send_header('Content-type','text/plain')
+    #     self.end_headers()
+    #     if "name" in dic:
+    #         message = "Hello, " + dic["name"] + "!"
+    #     else:
+    #         message = "Hello, stranger!"
+    #     self.wfile.write(message.encode())
+    #     return
+
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_data = json.loads(self.rfile.read(content_len))
+        tracking_id = post_data['tracking_id']
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
-        else:
-            message = "Hello, stranger!"
-        self.wfile.write(message.encode())
+        self.wfile.write(
+            json.dumps({"url": tracking_id}).encode()
+        )
         return
---
+++ api/ups.py
@@ -0,0 +1,29 @@
+# from ClassicUPS import UPSConnection
+
+
+# ups = UPSConnection('ADAC6AB1040D9A52',
+#                     'Y789W4',
+#                     'tH^3@N75H@4vI',
+#                     debug=True)
+
+# tracking = ups.tracking_info('1Z12345E0291980793')
+# # print (help(tracking))
+# print(tracking.delivered)
+# print(tracking.in_transit)
+# print(tracking.shipment_activities)
+
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+        self.wfile.write(message.encode())
+        return
