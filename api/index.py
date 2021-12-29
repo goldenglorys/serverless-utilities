286f8324ee3ec9d79fb8f9eea20ab6b2452b5c75
goldenglorys <olusolagloryolamide@gmail.com>
2021-12-29 22:53:53 +0100

    added for ups

--- api/index.py
+++ api/index.py
@@ -1,30 +1,22 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
 import json
+from ClassicUPS import UPSConnection
 
 
 class handler(BaseHTTPRequestHandler):
-    # def do_GET(self):
-    #     s = self.path
-    #     dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-    #     self.send_response(200)
-    #     self.send_header('Content-type','text/plain')
-    #     self.end_headers()
-    #     if "name" in dic:
-    #         message = "Hello, " + dic["name"] + "!"
-    #     else:
-    #         message = "Hello, stranger!"
-    #     self.wfile.write(message.encode())
-    #     return
-
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_data = json.loads(self.rfile.read(content_len))
         tracking_id = post_data['tracking_id']
+        ups = UPSConnection('ADAC6AB1040D9A52',
+                    'Y789W4',
+                    'tH^3@N75H@4vI',
+                    debug=True)
+        tracking = ups.tracking_info(tracking_id)
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(
-            json.dumps({"url": tracking_id}).encode()
+            json.dumps({"result": tracking}).encode()
         )
         return
