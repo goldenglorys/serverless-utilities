44a4946238d4f9a9d882d70235c1b79b5d8a7246
goldenglorys <olusolagloryolamide@gmail.com>
2022-01-06 05:01:42 +0100

    add get

--- api/index.py
+++ api/index.py
@@ -1,22 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 import json
 from ClassicUPS import UPSConnection
-
+import os
 
 class handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_len = int(self.headers.get('Content-Length'))
-        post_data = json.loads(self.rfile.read(content_len))
-        tracking_id = post_data['tracking_id']
-        ups = UPSConnection('ADAC6AB1040D9A52',
-                    'Y789W4',
-                    'tH^3@N75H@4vI',
-                    debug=True)
-        tracking = ups.tracking_info(tracking_id)
+    def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
+        self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write(
-            json.dumps({"result": tracking}).encode()
+            json.dumps(
+                {
+                    "DATABASE": os.getenv("DATABASE_URL"),
+                    "CREDS_JSON_STR": os.getenv("CREDS_JSON_STR")
+                }
+            )
         )
         return
